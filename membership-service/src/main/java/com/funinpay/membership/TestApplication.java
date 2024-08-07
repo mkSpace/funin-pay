649f8eb9ccfa05b3f6df372fb4188c22e69896e3
Jaemin.Park <once29@naver.com>
2024-08-08 01:41:40 +0900

    Add membership service module

---
+++ membership-service/src/main/java/com/funinpay/membership/TestApplication.java
@@ -0,0 +1,11 @@
+package com.funinpay.membership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
--- src/main/java/com/funinpay/Main.java
+++
@@ -1,7 +0,0 @@
-package com.funinpay;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
